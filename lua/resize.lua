5ea569bca0d6a4de460e966cfa07bd18dc2cde69
Bastien Souchu <bsouchu@gmail.com>
2026-03-13 13:45:57 +0100

    Fix go to previous window

--- lua/resize.lua
+++ lua/resize.lua
@@ -1,52 +1,51 @@
 local M = {}
 
-M.setup = function(opts)
-end
+M.setup = function(opts) end
 
 M.isRightMost = function()
 	local curWin = vim.fn.winnr()
-	vim.cmd [[wincmd l]]
+	vim.cmd([[wincmd l]])
 	local rightWin = vim.fn.winnr()
 	if curWin == rightWin then
 		return true
 	else
-		vim.cmd [[wincmd h]]
+		vim.cmd([[wincmd p]])
 		return false
 	end
 end
 
 M.isLeftMost = function()
 	local curWin = vim.fn.winnr()
-	vim.cmd [[wincmd h]]
+	vim.cmd([[wincmd h]])
 	local leftWin = vim.fn.winnr()
 	if curWin == leftWin then
 		return true
 	else
-		vim.cmd [[wincmd l]]
+		vim.cmd([[wincmd p]])
 		return false
 	end
 end
 
 M.isBottomMost = function()
 	local curWin = vim.fn.winnr()
-	vim.cmd [[wincmd j]]
+	vim.cmd([[wincmd j]])
 	local bottomWin = vim.fn.winnr()
 	if curWin == bottomWin then
 		return true
 	else
-		vim.cmd [[wincmd k]]
+		vim.cmd([[wincmd p]])
 		return false
 	end
 end
 
 M.isTopMost = function()
 	local curWin = vim.fn.winnr()
-	vim.cmd [[wincmd k]]
+	vim.cmd([[wincmd k]])
 	local topWin = vim.fn.winnr()
 	if curWin == topWin then
 		return true
 	else
-		vim.cmd [[wincmd j]]
+		vim.cmd([[wincmd p]])
 		return false
 	end
 end
@@ -54,44 +53,44 @@ end
 M.ResizeLeft = function()
 	if M.isRightMost() then
 		if not M.isLeftMost() then
-			vim.cmd [[wincmd 5 >]]
+			vim.cmd([[wincmd 5 >]])
 		end
 	else
-		vim.cmd [[wincmd 5 <]]
+		vim.cmd([[wincmd 5 <]])
 	end
 end
 
 M.ResizeRight = function()
 	if M.isRightMost() then
 		if not M.isLeftMost() then
-			vim.cmd [[wincmd 5 <]]
+			vim.cmd([[wincmd 5 <]])
 		end
 	else
-		vim.cmd [[wincmd 5 >]]
+		vim.cmd([[wincmd 5 >]])
 	end
 end
 
 M.ResizeUp = function()
 	if M.isBottomMost() then
 		if not M.isTopMost() then
-			vim.cmd [[wincmd 5 +]]
+			vim.cmd([[wincmd 5 +]])
 		else
-			vim.cmd [[wincmd 5 -]]
+			vim.cmd([[wincmd 5 -]])
 		end
 	else
-		vim.cmd [[wincmd 5 -]]
+		vim.cmd([[wincmd 5 -]])
 	end
 end
 
 M.ResizeDown = function()
 	if M.isBottomMost() then
 		if not M.isTopMost() then
-			vim.cmd [[wincmd 5 -]]
+			vim.cmd([[wincmd 5 -]])
 		else
-			vim.cmd [[wincmd 5 +]]
+			vim.cmd([[wincmd 5 +]])
 		end
 	else
-		vim.cmd [[wincmd 5 +]]
+		vim.cmd([[wincmd 5 +]])
 	end
 end
 
